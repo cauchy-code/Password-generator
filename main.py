234268f840a3dfbc26a1800e0c2219a03898812c
KASI VISWANATHAN K <101562306+cauchy-code@users.noreply.github.com>
2024-01-27 11:41:59 +0530

    Create main.py
    Python Password generator using random module

---
+++ main.py
@@ -0,0 +1,24 @@
+#Password Generator Project
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+L = random.sample(letters, nr_letters)
+S = random.sample(symbols, nr_symbols)
+N = random.sample(numbers, nr_numbers)
+easy_password = ''.join(L) + ''.join(S) + ''.join(N)
+print(easy_password)
+hard_password = L + S + N
+random.shuffle(hard_password)
+print(f"Hard password is {''.join(hard_password)} ")
